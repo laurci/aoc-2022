895ec1fec0e8011d68cd2b37a3cac2465ad2897e
Laurentiu Ciobanu <laurentiu.ciobanu@sessions.us>
2022-12-06 16:40:09 +0000

    cleanup day 2

--- day-2/target/debug/day-2.d
+++
@@ -1,1 +0,0 @@
-/home/sessions/laurci/aoc-2022/day-2/target/debug/day-2: /home/sessions/laurci/aoc-2022/day-2/src/main.rs
--- day-2/target/debug/deps/day_2-64407bcb2d006a45.d
+++
@@ -1,5 +0,0 @@
-/home/sessions/laurci/aoc-2022/day-2/target/debug/deps/day_2-64407bcb2d006a45.rmeta: src/main.rs
-
-/home/sessions/laurci/aoc-2022/day-2/target/debug/deps/day_2-64407bcb2d006a45.d: src/main.rs
-
-src/main.rs:
